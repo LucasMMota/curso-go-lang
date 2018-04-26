c685c68d81aea6a0e0c7c8b8a04d64010453b9cb
Lucas Mendes <lucas.lmmf@gmail.com>
2018-04-26 14:29:58 -0300

    aula 11

--- prints/prints.go
+++ prints/prints.go
@@ -5,4 +5,28 @@ import "fmt"
 func main() {
 	fmt.Print("Mesma")
 	fmt.Print(" linha")
+
+	fmt.Println(" Nova")
+	fmt.Println("linha.")
+
+	x := 3.141516
+
+	// fmt.Println("O valor de x é:" + x)
+
+	// conversao
+	xs := fmt.Sprint(x)
+	fmt.Println("O valor de x é:", xs)
+
+	// sem conversao
+	fmt.Println("O valor de x é:", x)
+
+	// print formatado
+	fmt.Printf("O valor de x é: %.2f", x)
+
+	a := 1
+	b := 1.9999
+	c := false
+	d := "opa"
+	fmt.Printf("\n%d %f %.1f %t %s", a, b, b, c, d)
+	fmt.Printf("\n%v %v %v %v", a, b, c, d) // com %v
 }
