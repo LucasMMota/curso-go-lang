2c6273e9a2904f8a8774092164e360238695dcec
Lucas Mendes <lucas.lmmf@gmail.com>
2018-05-02 17:14:51 -0300

    aula 87

---
+++ sql/update/update.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:@/curso_go")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close() // atrasa o fechamento da conexão
+
+	stmt, _ := db.Prepare("update usuarios set nome = ? where id = ?")
+
+	stmt.Exec("Hermione da Silva", 1)
+	stmt.Exec("Calvo Geraldo", 2)
+
+	stmt2, _ := db.Prepare("delete from usuarios where id = ?")
+	stmt2.Exec(3)
+}
