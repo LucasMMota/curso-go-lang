6ae05a9c66513175bbd32c38f8325317fa49a4a9
Lucas Mendes <lucas.lmmf@gmail.com>
2018-05-02 16:39:38 -0300

    aula 81

--- testes/tabela/strings_test.go
+++ testes/tabela/strings_test.go
@@ -8,6 +8,7 @@ import (
 const msgIndex = "%s (parte: %s) índices: esperado (%d) <> encontrado (%d)."
 
 func TestIndex(t *testing.T) {
+	t.Parallel()
 	testes := []struct { // define a struct
 		texto    string
 		parte    string
