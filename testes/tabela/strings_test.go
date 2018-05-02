44c3eb37b49db22b1469136ef718782969f35186
Lucas Mendes <lucas.lmmf@gmail.com>
2018-05-02 16:32:26 -0300

    aula 80 - testes

---
+++ testes/tabela/strings_test.go
@@ -0,0 +1,30 @@
+package strings
+
+import (
+	"strings"
+	"testing"
+)
+
+const msgIndex = "%s (parte: %s) índices: esperado (%d) <> encontrado (%d)."
+
+func TestIndex(t *testing.T) {
+	testes := []struct { // define a struct
+		texto    string
+		parte    string
+		esperado int
+	}{ // passa um data set
+		{"Lucas é show", "Lucas", 0},
+		{"", "", 0},
+		{"Opa", "opa", -1},
+		{"leonardo", "o", 2},
+	}
+
+	for _, teste := range testes {
+		t.Logf("Massa: %v", teste)
+		atual := strings.Index(teste.texto, teste.parte) // retorna a posicao da palavra na string
+
+		if atual != teste.esperado {
+			t.Errorf(msgIndex, teste.texto, teste.parte, teste.esperado, atual)
+		}
+	}
+}
